73f80e0f3ae37cd45fa5f97912ceb1776b4f586e
pxtri2156 <pxtri2156@gmail.com>
2023-08-30 14:04:58 +0700

    Some updates for visualize log

--- non_parametrics/notears/notears/nonlinear_wandb.py
+++ non_parametrics/notears/notears/nonlinear_wandb.py
@@ -1,8 +1,7 @@
 
 import  sys
 
-sys.path.append("./")
-
+sys.path.append("../")
 from notears.locally_connected import LocallyConnected
 from notears.lbfgsb_scipy import LBFGSBScipy
 from notears.trace_expm import trace_expm
@@ -29,11 +28,8 @@ class NotearsMLP(nn.Module):
         # fc1: variable splitting for l1
         self.fc1_pos = nn.Linear(d, d * dims[1], bias=bias) # d*dims[1] => 
         self.fc1_neg = nn.Linear(d, d * dims[1], bias=bias)
-        # print('weight fc1: ', self.fc1_pos.weight.size())
 
         self.fc1_pos.weight.bounds = self._bounds()
-        # print(self.fc1_pos.weight.bounds)
-
         self.fc1_neg.weight.bounds = self._bounds()
         # fc2: local linear layers
         layers = []
@@ -41,7 +37,6 @@ class NotearsMLP(nn.Module):
             layers.append(LocallyConnected(d, dims[l + 1], dims[l + 2], bias=bias))
 
         self.fc2 = nn.ModuleList(layers)
-        # print(type( self.fc2))
 
     def _bounds(self):
         d = self.dims[0]
@@ -63,7 +58,6 @@ class NotearsMLP(nn.Module):
         for fc in self.fc2:
             x = torch.sigmoid(x)  # [n, d, m1]
             x = fc(x)  # [n, d, m2]
-            # print('fc2 weight: ', fc.weight.size())
         x = x.squeeze(dim=2)  # [n, d]
         return x
 
@@ -71,16 +65,10 @@ class NotearsMLP(nn.Module):
         """Constrain 2-norm-squared of fc1 weights along m1 dim to be a DAG"""
         d = self.dims[0]
         fc1_weight = self.fc1_pos.weight - self.fc1_neg.weight  # [j * m1, i]
-        # print('fc1_weight: ', fc1_weight.shape)
         fc1_weight = fc1_weight.view(d, -1, d)  # [j, m1, i]
 
         A = torch.sum(fc1_weight * fc1_weight, dim=1).t()  # [i, j]
         h = trace_expm(A) - d  # (Zheng et al. 2018)
-        # A different formulation, slightly faster at the cost of numerical stability
-        # M = torch.eye(d) + A / d  # (Yu et al. 2019)
-        # E = torch.matrix_power(M, d - 1)
-        # h = (E.t() * M).sum() - d
-        # print("h function: ", h)
 
         return h
 
@@ -109,20 +97,17 @@ class NotearsMLP(nn.Module):
         W = W.cpu().detach().numpy()  # [i, j]
         return W
 
-
 def squared_loss(output, target):
     n = target.shape[0]
     loss = 0.5 / n * torch.sum((output - target) ** 2)
     return loss
 
-
-def dual_ascent_step(model, X, lambda1, lambda2, rho, alpha, h, rho_max):
+def dual_ascent_step(model, X, wandb, lambda1, lambda2, rho, alpha, h, rho_max):
     """Perform one step of dual ascent in augmented Lagrangian."""
     h_new = None
     optimizer = LBFGSBScipy(model.parameters())
     X_torch = torch.from_numpy(X)  
-    
-    loss = 0
+
     while rho < rho_max:
         def closure():
             optimizer.zero_grad()
@@ -134,32 +119,27 @@ def dual_ascent_step(model, X, lambda1, lambda2, rho, alpha, h, rho_max):
             l2_reg = 0.5 * lambda2 * model.l2_reg()
             l1_reg = lambda1 * model.fc1_l1_reg()
             primal_obj = loss + penalty + l2_reg + l1_reg
+            result_dict = {'obj_func': primal_obj, 
+                           'sq_loss': loss, 
+                           'penalty': penalty,
+                            'l1': l1_reg,
+                            'l2': l2_reg,
+                            'h_func': h_new}         
+            wandb.log(result_dict)
+            wandb.watch(model)
             primal_obj.backward()
             return primal_obj
         optimizer.step(closure)  # NOTE: updates model in-place
         
         with torch.no_grad():
             h_new = model.h_func().item()
-            X_hat = model(X_torch)
-            # print('X hat:', X_hat.shape)
-            loss = squared_loss(X_hat, X_torch)
-            penalty = 0.5 * rho * h_new * h_new + alpha * h_new
-            l2_reg = 0.5 * lambda2 * model.l2_reg()
-            l1_reg = lambda1 * model.fc1_l1_reg()
-            primal_obj = loss + penalty + l2_reg + l1_reg
-            result_dict = {'obj_func': primal_obj, 
-                           'sq_loss': loss, 
-                           'penalty': penalty,
-                            'l1': l1_reg,
-                            'l2': l2_reg,
-                            'h_func': h_new} #THE SAME KEY AS INIT IN MAIN FUNCTION
 
         if h_new > 0.25 * h:
             rho *= 10
         else:
             break
     alpha += rho * h_new
-    return rho, alpha, h_new, result_dict
+    return rho, alpha, h_new
 
 
 def notears_nonlinear(model: nn.Module,
@@ -173,9 +153,10 @@ def notears_nonlinear(model: nn.Module,
     rho, alpha, h = 1.0, 0.0, np.inf
     for _ in range(max_iter):
         print(f"{'='*30} Iter {_} {'='*30}")
-        rho, alpha, h, result_dict = dual_ascent_step(model, X, wandb, lambda1, lambda2,
+        rho, alpha, h = dual_ascent_step(model, X, wandb, lambda1, lambda2,
                                          rho, alpha, h, rho_max)
-        wandb.log(result_dict)
+        wandb.log({'rho': rho,
+                   'alpha': alpha})         
         if h <= h_tol or rho >= rho_max:
             break
     W_est = model.fc1_to_adj()
@@ -187,16 +168,17 @@ def main():
     # Log 
     torch.set_default_dtype(torch.double)
     np.set_printoptions(precision=3)
-    for r in range(1, 5):
+    for r in range(15):
+        name_seed = 'seed_' + str(r)
         ut.set_random_seed(r)
 
         wandb.init(
             # set the wandb project where this run will be logged
-            project="Causal Discovery",
-            
+            project="Nonliner",
+            name=name_seed,
             # track hyperparameters and run metadata
             config={
-            "name": "Non parametric" + str(r),
+            "name": name_seed,
             "dataset": "Synthetic",
             "lambda1": 0.01,
             "lambda2": 0.01,
@@ -211,14 +193,20 @@ def main():
         np.savetxt('X.csv', X, delimiter=',')
 
         model = NotearsMLP(dims=[d, 10, 1], bias=True)
+        params = sum(p.numel() for p in model.parameters())
+        print("params: ", params)
+        input("Stop")
+    
+        wandb.watch(model)
         W_est = notears_nonlinear(model, X, wandb, lambda1=0.01, lambda2=0.01)
         print(W_est)
         assert ut.is_dag(W_est)
         np.savetxt('W_est.csv', W_est, delimiter=',')
         acc = ut.count_accuracy(B_true, W_est != 0)
+        wandb.log({'acc': acc})
         print(acc)
         wandb.finish()
-
+        break
 
 
 if __name__ == '__main__':
